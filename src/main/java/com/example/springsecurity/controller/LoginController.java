493c5d7397dd2429d82803b3f5e68ae316afec82
daqianshijie999 <1355083406@qq.com>
2021-10-17 14:30:40 +0800

    第一次提交：SpringSecurity自定义登录

---
+++ src/main/java/com/example/springsecurity/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.example.springsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/login")
+    public String login() {
+        System.out.println("执行登录操作");
+        return "redirect:main.html";
+    }
+}
---
+++ src/test/java/com/example/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+//@SpringBootTest
+class SpringsecurityApplicationTests {
+
+    @Test
+    public void test() {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        System.out.println(passwordEncoder.encode("123456"));
+    }
+
+}
